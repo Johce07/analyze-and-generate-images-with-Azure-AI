efdcd630e579096fcd5415321136b97fc2e55584
Johce07 <105455276+Johce07@users.noreply.github.com>
2023-11-30 12:22:57 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  const handleAnalysis = () => {
+    // Lógica para el análisis de imágenes
+    console.log('Analizando la imagen:', imageUrl);
+    // Puedes agregar aquí la lógica para analizar la imagen
+  };
+
+  const handleImageGeneration = () => {
+    // Lógica para la generación de imágenes
+    console.log('Generando imagen para la solicitud:', imageUrl);
+    // Puedes agregar aquí la lógica para generar la imagen
+  };
+
+  return (
+    <div>
+      <h1>Image Analyzer</h1>
+      <label>
+        Dirección URL de la imagen:
+        <input
+          type="text"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleAnalysis}>Analizar Imagen</button>
+      <button onClick={handleImageGeneration}>Generar Imagen</button>
+    </div>
+  );
 }
 
 export default App;
